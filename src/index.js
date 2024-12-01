d6ca7d647226c49f91bfd3badd4104b0719d11c4
Vivek Singh Pundir <132738914+vivekdevp01@users.noreply.github.com>
2024-12-01 09:53:17 +0530

    Added

--- src/index.js
+++ src/index.js
@@ -1,32 +1,24 @@
-const express=require('express');
-const bodyParser = require('body-parser');
-const apiRouter=require('./routes');
+const express = require("express");
+const bodyParser = require("body-parser");
+const apiRouter = require("./routes");
 
+const { PORT } = require("./config/server.config");
+const errorHandler = require("./utils/errorHandler");
+const connectToDB = require("./config/db.config");
 
-const {PORT}=require('./config/server.config');
-const errorHandler = require('./utils/errorHandler');
-const connectToDB = require('./config/db.config');
-
-
-
-const app= express();
-app.use(bodyParser.urlencoded({extended: true}));
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 
-
-app.use('/api',apiRouter);
-app.get('/ping',(req,res)=>{
-    return res.json({message:"It is running"});
-})
+app.use("/api", apiRouter);
+app.get("/ping", (req, res) => {
+  return res.json({ message: "It is running" });
+});
 
 app.use(errorHandler);
-app.listen(PORT,async ()=>{
-    console.log(`Server listening on ${PORT}`);
-    await connectToDB();
-    console.log('Connected to MongoDB');
-})
-
-
-
-
+app.listen(PORT, async () => {
+  console.log(`Server listening on ${PORT}`);
+  await connectToDB();
+  console.log("Connected to MongoDB");
+});
